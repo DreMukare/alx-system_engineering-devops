5d834669bc78c890f403b6c182ef00c5bdd59fb8
Andrew Mukare <dremukare@gmail.com>
2021-07-06 21:25:05 +0300

    Update 100-textme.rb

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,4 +1,7 @@
 #!/usr/bin/env ruby
-# Matches a repeating token
+# [from:] [to:] [flags:]
+# The sender phone number or name (including country code if present)
+# The receiver phone number or name (including country code if present)
+# The flags that were used
 
 puts ARGV[0].scan(/\[from:(\w+|+\d{11,11})\]\s\[to:(\w+|+\d{10,10})\]\s\[flags:-?\d:-?\d:-?\d:-?\d:-?\d\]/).join
