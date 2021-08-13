688e4f91a28b5e651895f442a15c3bb457c355fa
Andrew Mukare <dremukare@gmail.com>
2021-08-13 19:36:50 +0000

    task 0

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,9 @@
+# creating a file in /tmp with specific requirements
+
 file {'holberton':
     path => '/tmp/holberton',
-    ensure => 'file',
     content => 'I love Puppet',
     owner => 'www-data',
     group => 'www-data',
-    mode => '0744'
+    mode => '0744',
 }
