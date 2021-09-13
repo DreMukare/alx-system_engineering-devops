7efed995b3b87ab57b6af0cdf5e29740a869917f
Andrew Mukare <dremukare@gmail.com>
2021-09-13 18:45:10 +0000

    task 2

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -9,12 +9,9 @@ if __name__ == '__main__':
     url = 'https://jsonplaceholder.typicode.com/'
     user_data = requests.get(url + 'users/{}'.format(argv[1])).json()
     user_tasks = requests.get(url + 'todos', params={'userId': argv[1]}).json()
-    file_dict = {str(argv[1]): []}
-    out_dict = {}
     with open('{}.json'.format(argv[1]), 'w') as f:
-        for task in user_tasks:
-            out_dict['task'] = task.get('title')
-            out_dict['completed'] = task.get('completed')
-            out_dict['username'] = user_data.get('username')
-            file_dict[str(argv[1])].append(out_dict)
-        json.dump(file_dict, f)
+        json.dump({argv[1]: [{
+            "task": task.get('title'),
+            "completed": task.get('completed'),
+            "username": user_data.get('username')
+        } for task in user_tasks]}, f)
