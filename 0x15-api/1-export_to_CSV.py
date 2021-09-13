570cef773e6aea592e9622f7b167d1494e95db07
Andrew Mukare <dremukare@gmail.com>
2021-09-13 18:15:32 +0000

    task 1

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+''' makes a get request to a REST API '''
+import requests
+from sys import argv
+
+
+if __name__ == '__main__':
+    url = 'https://jsonplaceholder.typicode.com/'
+    user_data = requests.get(url + 'users/{}'.format(argv[1])).json()
+    user_tasks = requests.get(url + 'todos', params={'userId': argv[1]}).json()
+    user_name = user_data.get('username')
+    with open('{}.csv'.format(argv[1]), 'w') as f:
+        for task in user_tasks:
+            line = '"{}","{}","{}","{}"\n'.format(
+                task.get('userId'),
+                user_name,
+                task.get('completed'),
+                task.get('title'))
+            f.write(line)
