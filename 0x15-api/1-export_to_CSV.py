8c8e2a0b7dba0d6c909f30ca7b94755f148e5749
Andrew Mukare <dremukare@gmail.com>
2021-09-13 18:18:25 +0000

    task 1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -8,12 +8,11 @@ if __name__ == '__main__':
     url = 'https://jsonplaceholder.typicode.com/'
     user_data = requests.get(url + 'users/{}'.format(argv[1])).json()
     user_tasks = requests.get(url + 'todos', params={'userId': argv[1]}).json()
-    user_name = user_data.get('username')
     with open('{}.csv'.format(argv[1]), 'w') as f:
         for task in user_tasks:
             line = '"{}","{}","{}","{}"\n'.format(
                 task.get('userId'),
-                user_name,
+                user_data.get('username'),
                 task.get('completed'),
                 task.get('title'))
             f.write(line)
